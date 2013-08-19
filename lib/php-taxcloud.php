514ba4859a9218866c5204ced25452c3354c07cd
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2013-08-19 09:59:38 -0500

    fix typo

--- lib/php-taxcloud.php
+++ lib/php-taxcloud.php
@@ -8,7 +8,7 @@
 require_once(__DIR__ . '/Autoload.php');
 
 $classLoader = new ClassLoader;
-$classLoader->registerNamespace(array(
+$classLoader->registerNamespaces(array(
   'TaxCloud' => array(__DIR__, __DIR__ . '/../tests')
 ));
 $classLoader->register();
