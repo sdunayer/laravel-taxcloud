868c862c5a22b2fb15809d3e748f7dacf45e6d34
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2013-08-20 21:32:46 -0500

    update smoketest

--- smoketest.php
+++ smoketest.php
@@ -9,9 +9,13 @@
  */
 
 // API credentials loaded from environment variables.
-$apiLoginID = $_ENV["TaxCloud_apiLoginID"] . 'xxx';
+$apiLoginID = $_ENV["TaxCloud_apiLoginID"];
 $apiKey = $_ENV["TaxCloud_apiKey"];
 $uspsUserID = $_ENV["TaxCloud_uspsUserID"];
+
+// Some variable that need to be unique, but can't change.
+$orderID = rand();
+$cartID = rand(1, 999);
 
 /**
  * Show us what step we are on.
@@ -31,6 +35,27 @@ $pingParams->apiLoginID = $apiLoginID;
 $pingParams->apiKey = $apiKey;
 
 print_r($client->Ping($pingParams));
+
+step('Get TICs');
+
+$params = new \TaxCloud\GetTICs($apiLoginID, $apiKey);
+print_r($client->GetTICs($params));
+
+step('Cart Item');
+$cartItems = array();
+$cartItem = new \TaxCloud\CartItem($cartID + 1, 'ABC123', '00000', 12.00, 1);
+$cartItems[] = $cartItem;
+print_r($cartItem);
+
+step('Cart Item - Shipping');
+
+$cartItemShipping = new \TaxCloud\CartItem($cartID + 2, 'SHIPPING123', 11010, 8.95, 1);
+$cartItems[] = $cartItemShipping;
+print_r($cartItemShipping);
+
+step('Cart Items Array');
+
+print_r($cartItems);
 
 step('Verify Address');
 $address = new \TaxCloud\Address;
@@ -52,4 +77,37 @@ $verifyAddress->state = $address->getState();
 $verifyAddress->zip5 = $address->getZip5();
 $verifyAddress->zip4 = $address->getZip4();
 
-print_r($client->VerifyAddress($verifyAddress));
+$verifiedAddress = $client->VerifyAddress($verifyAddress);
+print_r($verifiedAddress);
+
+step('Lookup');
+
+print_r($address);
+
+$originAddress = new \TaxCloud\Address();
+$originAddress->setAddress1($verifiedAddress->VerifyAddressResult->Address1);
+$originAddress->setCity($verifiedAddress->VerifyAddressResult->City);
+$originAddress->setState($verifiedAddress->VerifyAddressResult->State);
+$originAddress->setZip5($verifiedAddress->VerifyAddressResult->Zip5);
+$originAddress->setZip4($verifiedAddress->VerifyAddressResult->Zip4);
+
+$destAddress = new \TaxCloud\Address();
+$destAddress->setAddress1('PO Box 573');
+$destAddress->setCity('Clinton');
+$destAddress->setState('OK');
+$destAddress->setZip5('73601');
+
+print_r($destAddress);
+
+$lookup = new \TaxCloud\Lookup($apiLoginID, $apiKey, '123', $cartID, $cartItems, $originAddress, $destAddress);
+print_r($client->Lookup($lookup));
+
+step('Authorized');
+
+$authorization = new \TaxCloud\Authorized($apiLoginID, $apiKey, '123', $cartID, $cartItems, $orderID, date("c"));
+print_r($client->Authorized($authorization));
+
+step('Captured');
+$capture = new \TaxCloud\Captured($apiLoginID, $apiKey, $orderID);
+print_r($client->Captured($capture));
+
