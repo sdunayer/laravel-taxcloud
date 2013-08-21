fefd71dc9a2fcf83d82f8d56d371c3d346e4ab5d
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2013-08-21 00:57:59 -0500

    added setters and getters to VerifyAddress

--- lib/TaxCloud/VerifyAddress.php
+++ lib/TaxCloud/VerifyAddress.php
@@ -17,13 +17,13 @@ class VerifyAddress {
   private $zip4;
 
   public function __construct($uspsUserID, Address $address) {
-    $this->uspsUserID = $uspsUserID;
-    $this->address1 = $address->getAddress1();
-    $this->address2 = $address->getAddress2();
-    $this->city = $address->getCity();
-    $this->state = $address->getState();
-    $this->zip5 = $address->getZip5();
-    $this->zip4 = $address->getZip4();
+    $this->setUspsUserID($uspsUserID);
+    $this->setAddress1($address->getAddress1());
+    $this->setAddress2($address->getAddress2());
+    $this->setCity($address->getCity());
+    $this->setState($address->getState());
+    $this->setZip5($address->getZip5());
+    $this->setZip4($address->getZip4());
   }
 
   public function setUspsUserID($uspsUserID) {
@@ -32,5 +32,53 @@ class VerifyAddress {
 
   public function getUspsUserID() {
     return $this->uspsUserID;
+  }
+
+  private function setAddress1($address1) {
+    $this->address1 = $address1;
+  }
+
+  public function getAddress1() {
+    return $this->address1;
+  }
+
+  private function setAddress2($address2) {
+    $this->address2 = $address2;
+  }
+
+  public function getAddress2() {
+    return $this->address2;
+  }
+
+  private function setCity($city) {
+    $this->city = $city;
+  }
+
+  public function getCity() {
+    return $this->city;
+  }
+
+  private function setState($state) {
+    $this->state = $state;
+  }
+
+  public function getState() {
+    return $this->state;
+  }
+
+  private function setZip5($zip5) {
+    $this->zip5 = $zip5;
+  }
+
+  public function getZip5() {
+    return $this->zip5;
+  }
+
+  private function setZip4($zip4) {
+    $this->zip4 = $zip4;
+  }
+
+  public function getZip4() {
+    return $this->zip4;
   }
 }
